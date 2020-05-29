99a47dee3f7745f0eaaed59f5db31c284cb71b05
sampletext32 <sampletext32@bk.ru>
2020-05-29 15:40:05 +0300

    Fixup

--- Entities/Album.cs
+++ Entities/Album.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Entities
 {
     public class Album : Playlist
     {
         public int Year { get; set; }
-        public ICollection<string> Artists { get; set; }
+        public ICollection<int> ArtistsId { get; set; }
 
-        public Album(int id, string title, ICollection<int> tracksIds, int ownerId, int artId, int year, ICollection<string> artists) : base(id, title, tracksIds, ownerId, artId)
+        public Album(int id, string title, ICollection<int> tracksIds, int ownerId, int artId, int year, ICollection<int> artistsId) : base(id, title, tracksIds, ownerId, artId)
         {
             Year = year;
-            Artists = artists;
+            ArtistsId = artistsId;
         }
     }
 }
