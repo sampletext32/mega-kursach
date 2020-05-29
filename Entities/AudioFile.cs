d50e2050ce793dcf048996aea3ca03a548789f4a
comougi <spsprwrw@gmail.com>
2020-05-29 12:04:48 +0300

    add AudioFile.cs

---
+++ Entities/AudioFile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Entities
+{
+    public class AudioFile : Entity
+    {
+        public List<string> Artists { get; set; }
+        public int GenreId { get; set; }
+        public int ArtId { get; set; }
+
+        public AudioFile(int id, string title, List<string> artists, int genreId, int artId) : base(id, title)
+        {
+            Artists = artists;
+            GenreId = genreId;
+            ArtId = artId;
+        }
+    }
+}
