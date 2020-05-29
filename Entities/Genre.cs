9cd6346084f3c2c1e4de2cb595e3b41d3fa18482
comougi <spsprwrw@gmail.com>
2020-05-29 11:40:59 +0300

    add Genre.cs

---
+++ Entities/Genre.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities
+{
+    public class Genre : Entity
+    {
+        public Genre(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
