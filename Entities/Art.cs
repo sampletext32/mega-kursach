588d57e1602e6b59d0cf03d1528debd7e9eba1e8
comougi <spsprwrw@gmail.com>
2020-05-29 11:55:33 +0300

    modify Art.cs

--- Entities/Art.cs
+++ Entities/Art.cs
@@ -7,5 +7,11 @@ namespace Entities
     {
         public int Id { get; set; }
         public string PicPath { get; set; }
+
+        public Art(int id, string picPath)
+        {
+            Id = id;
+            PicPath = picPath;
+        }
     }
 }
