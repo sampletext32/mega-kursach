db321e22c6dd1636013f54a33ee69a0d4a2df515
comougi <spsprwrw@gmail.com>
2020-05-29 11:54:59 +0300

    add Playlist.cs

---
+++ Entities/Playlist.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Entities
+{
+    public class Playlist : Entity
+    {
+        public List<int> TracksIds { get; set; }
+        public int OwnerId { get; set; }
+        public int ArtId { get; set; }
+
+
+        public Playlist(int id, string title, List<int> tracksIds, int ownerId, int artId)
+        {
+            Id = id;
+            Title = Title;
+            TracksIds = tracksIds;
+            OwnerId = ownerId;
+            ArtId = artId;
+        }
+    }
+}
