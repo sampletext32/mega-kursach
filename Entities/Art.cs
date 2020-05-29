527e3e67153cf0ca92500cd0f5fadc9333c8f8ec
comougi <spsprwrw@gmail.com>
2020-05-29 11:44:48 +0300

    add Art.cs

---
+++ Entities/Art.cs
@@ -0,0 +1,11 @@
+﻿using System.Dynamic;
+using System.Net.Mime;
+
+namespace Entities
+{
+    public class Art
+    {
+        public int Id { get; set; }
+        public string PicPath { get; set; }
+    }
+}
