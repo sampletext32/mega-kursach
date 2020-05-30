dfe10189f990b0ceae43b5b34c54f0710f2cf5e8
comougi <spsprwrw@gmail.com>
2020-05-30 17:50:34 +0300

    add Distributor.cs

---
+++ Entities/Distributor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Entities
+{
+    public class Distributor : User
+    {
+        public ICollection<Artist> DistributionArtists { get; set; }
+        public ICollection<Album> DistributionAlbums { get; set; }
+        public ICollection<Track> DistributionTracks { get; set; }
+
+        public Distributor(int id, string title, string password, string email, ICollection<Artist> distributionArtists,
+            ICollection<Album> distributionAlbums, ICollection<Track> distributionTracks) : base(id, title, password,
+            email)
+        {
+            DistributionArtists = distributionArtists;
+            DistributionAlbums = distributionAlbums;
+            DistributionTracks = distributionTracks;
+        }
+    }
+}
