3fc16a62595889e6f24a356633abbab4c965243d
comougi <spsprwrw@gmail.com>
2020-05-30 14:38:06 +0300

    add IGenreRepository.cs interface

---
+++ Infrastucture/IGenreRepository.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+
+namespace Infrastucture
+{
+    public interface IGenreRepository
+    {
+        Genre Get(int id);
+
+        void Add(Genre genre);
+        void Update(Genre genre);
+        void Remove(Genre genre);
+        
+        
+    }
+}
