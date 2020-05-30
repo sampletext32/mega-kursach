15c9539a117e82ed2408fe2b74e41b878102fcae
comougi <spsprwrw@gmail.com>
2020-05-30 12:53:11 +0300

    add IArtRepository.cs interface and update Art entity

--- Entities/Art.cs
+++ Entities/Art.cs
@@ -6,10 +6,12 @@ namespace Entities
     public class Art : AuditableEntity
     {
         public string PicPath { get; set; }
+        public int AlbumId { get; set; }
 
-        public Art(int id, string picPath) : base(id)
+        public Art(int id, string picPath, int albumId) : base(id)
         {
             PicPath = picPath;
+            AlbumId = albumId;
         }
     }
 }
---
+++ Infrastucture/IArtRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Entities;
+
+namespace Infrastucture
+{
+    public interface IArtRepository
+    {
+        Art Get(int id);
+
+        void Add(Art art);
+        void Update(Art art);
+    }
+}
