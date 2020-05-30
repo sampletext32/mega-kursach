3f8b47ffc5d0adfc5daf64d430e2d3f9a72c5310
comougi <spsprwrw@gmail.com>
2020-05-30 17:52:31 +0300

    add IClientRepository.cs interface

---
+++ Infrastucture/IClientRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+
+namespace Infrastucture
+{
+    public interface IClientRepository
+    {
+        Client Get(int id);
+
+        void Add(Client client);
+        void Update(Client client);
+        void Remove(Client client);
+    }
+}
