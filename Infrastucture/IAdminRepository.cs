6367e45106f60a1ca4dde7da9033c12227d5e2b9
comougi <spsprwrw@gmail.com>
2020-05-30 18:14:51 +0300

    add IAdminRepository.cs interface

---
+++ Infrastucture/IAdminRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+
+namespace Infrastucture
+{
+    public interface IAdminRepository
+    {
+        Admin Get(int id);
+
+        void Add(Admin admin);
+        void Update(Admin admin);
+        void Remove(Admin admin);
+    }
+}
