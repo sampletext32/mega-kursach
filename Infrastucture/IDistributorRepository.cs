9a2a5b3241d2f7863468cc3554adae4ccdbbcb46
comougi <spsprwrw@gmail.com>
2020-05-30 17:55:31 +0300

    add IDistributorRepository.cs interface

---
+++ Infrastucture/IDistributorRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Entities;
+
+namespace Infrastucture
+{
+    public interface IDistributorRepository
+    {
+        Distributor Get(int id);
+
+        void Add(Distributor distributor);
+        void Update(Distributor distributor);
+        void Remove(Distributor distributor);
+
+        IReadOnlyList<Distributor> GetDistributionAlbums(ICollection<Album> distributionAlbums);
+        IReadOnlyList<Distributor> GetDistributionArtists(ICollection<Artist> distributionArtists);
+        IReadOnlyList<Distributor> GetDistributionTracks(ICollection<Track> distributionTracks);
+    }
+}
