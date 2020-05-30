74f9d4a65f8d986a514a2c2733424899a84f52d9
comougi <spsprwrw@gmail.com>
2020-05-30 15:16:42 +0300

    update Playlist.cs entity

--- Entities/Playlist.cs
+++ Entities/Playlist.cs
@@ -9,20 +9,20 @@ namespace Entities
         public ICollection<int> TracksIds { get; set; }
 
         //Данное поле не будет хранится в БД
-        //[NotMapped]
-        //public ICollection<Track> Tracks => null;
+        [NotMapped]
+        public ICollection<Track> Tracks;
 
         public int OwnerId { get; set; }
 
         //Данное поле не будет хранится в БД
-        //[NotMapped]
-        //public User Owner => null;
+        [NotMapped]
+        public User Owner;
 
         public int ArtId { get; set; }
 
         //Данное поле не будет хранится в БД
-        //[NotMapped]
-        //public Art Art => null;
+        [NotMapped]
+        public Art Art;
 
         public Playlist(int id, string title, ICollection<int> tracksIds, int ownerId, int artId) : base(id, title)
         {
