547503f7077ab710f07539e4ee5e44896cb9960b
comougi <spsprwrw@gmail.com>
2020-05-30 12:35:22 +0300

    add AlbumRepository.cs and IAlbumRepository.cs interface

---
+++ Infrastucture/AlbumRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Entities;
+
+namespace Infrastucture
+{
+    public class AlbumRepository : AuditableEntityRepository<Album>, IAlbumRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public AlbumRepository(AppDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IReadOnlyList<Album> GetAlbumsByYear(int year)
+        {
+            return _dbContext.Albums.Where(x => x.Year.Equals(year)).ToList();
+        }
+
+        public IReadOnlyList<Album> GetAlbumsByArtist(int artistId)
+        {
+            return _dbContext.Albums.Where(x => x.ArtistsId.Equals(artistId)).ToList();
+        }
+
+        public IReadOnlyList<Album> GetAlbumsByTitle(string title)
+        {
+            return _dbContext.Albums.Where(x => x.Title.Contains(title)).ToList();
+        }
+
+        public IReadOnlyList<Album> GetAllAlbums()
+        {
+            return _dbContext.Albums.ToList();
+        }
+    }
+}
---
+++ Infrastucture/IAlbumRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Entities;
+
+namespace Infrastucture
+{
+    public interface IAlbumRepository
+    {
+        Album Get(int id);
+
+        void Add(Album album);
+        void Update(Album album);
+
+        IReadOnlyList<Album> GetAlbumsByYear(int year);
+        IReadOnlyList<Album> GetAlbumsByArtist(int artistId);
+        IReadOnlyList<Album> GetAlbumsByTitle(string title);
+        IReadOnlyList<Album> GetAllAlbums();
+    }
+}
