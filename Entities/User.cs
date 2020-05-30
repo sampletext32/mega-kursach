489d8dfa50da22b08e5803e9dc470d07b7184105
comougi <spsprwrw@gmail.com>
2020-05-30 17:31:24 +0300

    add UserRoles enum

--- Entities/User.cs
+++ Entities/User.cs
@@ -4,6 +4,14 @@ using System.Text;
 
 namespace Entities
 {
+    enum UserRoles
+    {
+        Admin = 1,
+        Distributor = 2,
+        Artist = 3,
+        Client = 4
+    }
+
     public class User : TitledEntity
     {
         public User(int id, string title) : base(id, title)
