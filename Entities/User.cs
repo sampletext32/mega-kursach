4c773233a27ba4fecfa59589f5e04a79fedd3bd9
sampletext32 <sampletext32@bk.ru>
2020-05-30 16:03:39 +0300

    Add user

---
+++ Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class User : TitledEntity
+    {
+        public User(int id, string title) : base(id, title)
+        {
+        }
+    }
+}
