8f8174097a2b8bfc1520891ae6b399c208d95e7c
sampletext32 <sampletext32@bk.ru>
2020-06-02 15:36:07 +0300

    Add Db to ASP project

--- WebAsp/Startup.cs
+++ WebAsp/Startup.cs
@@ -1,5 +1,7 @@
+using Infrastucture;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,7 +20,13 @@ namespace WebAsp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
+            services.AddControllersWithViews();
+
+            //services.AddScoped<IBookRepository, BookRepository>();
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MsSqlConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
