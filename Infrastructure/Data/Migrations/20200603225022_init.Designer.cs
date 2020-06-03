f620a094e35f1c482f15b6a27ae5a718cbdf3941
sampletext32 <sampletext32@bk.ru>
2020-06-04 02:03:50 +0300

    Update migration

--- Infrastructure/Data/Migrations/20200603225022_init.Designer.cs
+++ Infrastructure/Data/Migrations/20200603225022_init.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20200602200515_init")]
+    [Migration("20200603225022_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
