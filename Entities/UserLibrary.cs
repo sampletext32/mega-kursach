09fc527efc2322929829b186384454fa23173e2c
sampletext32 <sampletext32@bk.ru>
2020-06-10 01:55:47 +0300

    Fix one-to-one relation

--- Entities/UserLibrary.cs
+++ Entities/UserLibrary.cs
@@ -5,13 +5,11 @@ namespace Entities
     public class UserLibrary
     {
         public int Id { get; set; }
-        public virtual User User { get; set; }
         public virtual ICollection<Album> Albums { get; set; }
         public virtual ICollection<Playlist> Playlists { get; set; }
 
-        public UserLibrary(User user, ICollection<Album> albums, ICollection<Playlist> playlists)
+        public UserLibrary(ICollection<Album> albums, ICollection<Playlist> playlists)
         {
-            User = user;
             Albums = albums;
             Playlists = playlists;
         }
