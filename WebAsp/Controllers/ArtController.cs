9abd7f5ad25cd5af62541b166e2e984f58995ecf
comougi <spsprwrw@gmail.com>
2020-06-12 13:16:44 +0300

    add ArtController.cs and HtmlArtController.cs

---
+++ WebAsp/Controllers/ArtController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtController : ControllerBase
+    {
+        private IArtRepository _repository;
+
+        public ArtController(IArtRepository artRepository)
+        {
+            _repository = artRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Art> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public Art Get(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Art value)
+        {
+            _repository.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Art value)
+        {
+            value.Id = id;
+            _repository.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Remove(_repository.GetById(id));
+        }
+    }
+}
---
+++ WebAsp/Controllers/HtmlArtController.cs
@@ -0,0 +1,86 @@
+﻿using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Route("html/[controller")]
+    public class HtmlArtController : Controller
+    {
+        private IArtRepository _repository;
+
+        public HtmlArtController(IArtRepository artRepository)
+        {
+            _repository = artRepository;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var arts = _repository.GetAll();
+            return View(arts);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Details(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+        [HttpGet("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm] Art art)
+        {
+            try
+            {
+                _repository.Add(art);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+        [HttpPost("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [FromForm] Art art)
+        {
+            try
+            {
+                _repository.Update(art);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpDelete("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            Art art = _repository.GetById(id);
+            if (art != null)
+            {
+                _repository.Remove(art);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
