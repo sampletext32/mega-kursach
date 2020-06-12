482b824443d8b2888b6ef6512003197728475860
comougi <spsprwrw@gmail.com>
2020-06-12 14:06:06 +0300

    add UserLibraryController and HtmlUserLibraryController

---
+++ WebAsp/Controllers/HtmlUserLibraryController.cs
@@ -0,0 +1,86 @@
+﻿using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Route("html/[controller]")]
+    public class HtmlUserLibraryController : Controller
+    {
+        private IUserLibraryRepository _repository;
+
+        public HtmlUserLibraryController(IUserLibraryRepository userLibraryRepository)
+        {
+            _repository = userLibraryRepository;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var libraries = _repository.GetAll();
+            return View(libraries);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Details(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+        [HttpGet("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm] UserLibrary library)
+        {
+            try
+            {
+                _repository.Add(library);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+        [HttpPost("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [FromForm] UserLibrary library)
+        {
+            try
+            {
+                _repository.Update(library);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpDelete("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            UserLibrary library = _repository.GetById(id);
+            if (library != null)
+            {
+                _repository.Remove(library);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebAsp/Controllers/UserLibraryController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class UserLibraryController : ControllerBase
+    {
+        private IUserLibraryRepository _repository;
+
+        public UserLibraryController(IUserLibraryRepository userLibraryRepository)
+        {
+            _repository = userLibraryRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<UserLibrary> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public UserLibrary Get(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] UserLibrary value)
+        {
+            _repository.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] UserLibrary value)
+        {
+            value.Id = id;
+            _repository.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Remove(_repository.GetById(id));
+        }
+    }
+}
