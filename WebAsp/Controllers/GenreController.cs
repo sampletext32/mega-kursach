b374ef09b24ab38e21c82a04f742231ca7dc7aab
comougi <spsprwrw@gmail.com>
2020-06-12 12:50:21 +0300

    add GenreController.cs and HtmlGenreController.cs

---
+++ WebAsp/Controllers/GenreController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        private IGenreRepository _repository;
+
+        public GenreController(IGenreRepository genreRepository)
+        {
+            _repository = genreRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Genre> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public Genre Get(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Genre value)
+        {
+            _repository.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Genre value)
+        {
+            value.Id = id;
+            _repository.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Remove(_repository.GetById(id));
+        }
+    }
+}
---
+++ WebAsp/Controllers/HtmlGenreController.cs
@@ -0,0 +1,80 @@
+﻿using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Route("html/[controller]")]
+    public class HtmlGenreController : Controller
+    {
+        private IGenreRepository _repository;
+
+        public HtmlGenreController(IGenreRepository genreRepository)
+        {
+            _repository = genreRepository;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var tracks = _repository.GetAll();
+            return View(tracks);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Details(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm] Genre genre)
+        {
+            try
+            {
+                _repository.Add(genre);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+        // POST: HtmlCountryController/Edit/5
+        [HttpPost("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [FromForm] Genre genre)
+        {
+            try
+            {
+                _repository.Update(genre);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpDelete("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            Genre genre = _repository.GetById(id);
+            if (genre != null)
+            {
+                _repository.Remove(genre);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
