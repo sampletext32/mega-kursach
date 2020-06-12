640b96c264563e318133ba9d0e24e2c3b349e355
comougi <spsprwrw@gmail.com>
2020-06-12 13:52:17 +0300

    add ArtistDataController.cs and HtmlArtistDataController.cs

---
+++ WebAsp/Controllers/ArtistDataController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class ArtistDataController : ControllerBase
+    {
+        private IArtistDataRepository _repository;
+
+        public ArtistDataController(IArtistDataRepository artistDataRepository)
+        {
+            _repository = artistDataRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<ArtistData> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ArtistData Get(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] ArtistData value)
+        {
+            _repository.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] ArtistData value)
+        {
+            value.Id = id;
+            _repository.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Remove(_repository.GetById(id));
+        }
+    }
+}
---
+++ WebAsp/Controllers/HtmlArtistDataController.cs
@@ -0,0 +1,86 @@
+﻿using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Route("html/[controller")]
+    public class HtmlArtistDataController : Controller
+    {
+        private IArtistDataRepository _repository;
+
+        public HtmlArtistDataController(IArtistDataRepository artistDataRepository)
+        {
+            _repository = artistDataRepository;
+        }
+        
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var artists = _repository.GetAll();
+            return View(artists);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Details(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+        [HttpGet("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm] ArtistData artist)
+        {
+            try
+            {
+                _repository.Add(artist);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+        [HttpPost("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [FromForm] ArtistData artist)
+        {
+            try
+            {
+                _repository.Update(artist);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpDelete("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            ArtistData artist = _repository.GetById(id);
+            if (artist != null)
+            {
+                _repository.Remove(artist);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
