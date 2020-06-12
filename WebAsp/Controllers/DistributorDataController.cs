b78deda5e1bfe5e73c5a8cb9d3ec353c09445bc8
comougi <spsprwrw@gmail.com>
2020-06-12 14:01:38 +0300

    add DistributorDataController and HtmlDistributorDataController

---
+++ WebAsp/Controllers/DistributorDataController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DistributorDataController : ControllerBase
+    {
+        private IDistributorDataRepository _repository;
+
+        public DistributorDataController(IDistributorDataRepository distributorDataRepository)
+        {
+            _repository = distributorDataRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<DistributorData> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public DistributorData Get(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] DistributorData value)
+        {
+            _repository.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] DistributorData value)
+        {
+            value.Id = id;
+            _repository.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Remove(_repository.GetById(id));
+        }
+    }
+}
---
+++ WebAsp/Controllers/HtmlDistributorDataController.cs
@@ -0,0 +1,86 @@
+﻿using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Route("api/[controller]")]
+    public class HtmlDistributorDataController : Controller
+    {
+        private IDistributorDataRepository _repository;
+
+        public HtmlDistributorDataController(IDistributorDataRepository distributorDataRepository)
+        {
+            _repository = distributorDataRepository;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var distributors = _repository.GetAll();
+            return View(distributors);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Details(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+        [HttpGet("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm] DistributorData distributors)
+        {
+            try
+            {
+                _repository.Add(distributors);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+        [HttpPost("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [FromForm] DistributorData distributor)
+        {
+            try
+            {
+                _repository.Update(distributor);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpDelete("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            DistributorData distributor = _repository.GetById(id);
+            if (distributor != null)
+            {
+                _repository.Remove(distributor);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
