bd20f06050bec88dccc2ab7dc1fced11924e22dc
comougi <spsprwrw@gmail.com>
2020-06-12 12:31:40 +0300

    add TrackController.cs and HtmlTrackController.cs

---
+++ WebAsp/Controllers/HtmlTrackController.cs
@@ -0,0 +1,87 @@
+﻿using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("html/[controller]")]
+    public class HtmlTrackController : Controller
+    {
+        private ITrackRepository _repository;
+
+        public HtmlTrackController(ITrackRepository trackRepository)
+        {
+            _repository = trackRepository;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var tracks = _repository.GetAll();
+            return View(tracks);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Details(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+        [HttpGet("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm] Track track)
+        {
+            try
+            {
+                _repository.Add(track);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        [HttpGet("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+
+        [HttpPost("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [FromForm] Track track)
+        {
+            try
+            {
+                _repository.Update(track);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        [HttpDelete("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            Track track = _repository.GetById(id);
+            if (track != null)
+            {
+                _repository.Remove(track);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebAsp/Controllers/TrackController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackController : ControllerBase
+    {
+        private ITrackRepository _repository;
+
+        public TrackController(ITrackRepository trackRepository)
+        {
+            _repository = trackRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Track> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public Track Get(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Track value)
+        {
+            _repository.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Track value)
+        {
+            value.Id = id;
+            _repository.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Remove(_repository.GetById(id));
+        }
+    }
+}
