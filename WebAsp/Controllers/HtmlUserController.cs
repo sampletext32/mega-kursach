833d9c817036b49d09ea9cc2103913fbd96701e3
comougi <spsprwrw@gmail.com>
2020-06-12 13:40:12 +0300

    add UserController.cs and HtmlUserController.cs

---
+++ WebAsp/Controllers/HtmlUserController.cs
@@ -0,0 +1,86 @@
+﻿using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Route("html/[controller")]
+    public class HtmlUserController : Controller
+    {
+        private IUserRepository _repository;
+
+        public HtmlUserController(IUserRepository userRepository)
+        {
+            _repository = userRepository;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var users = _repository.GetAll();
+            return View(users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Details(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+        [HttpGet("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm] User user)
+        {
+            try
+            {
+                _repository.Add(user);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            return View(_repository.GetById(id));
+        }
+
+        [HttpPost("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [FromForm] User user)
+        {
+            try
+            {
+                _repository.Update(user);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpDelete("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            User user = _repository.GetById(id);
+            if (user != null)
+            {
+                _repository.Remove(user);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebAsp/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Entities;
+using Infrastructure.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAsp.Controllers
+{
+    [Route("apt/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserRepository _repository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _repository = userRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] User value)
+        {
+            _repository.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] User value)
+        {
+            value.Id = id;
+            _repository.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Remove(_repository.GetById(id));
+        }
+    }
+}
