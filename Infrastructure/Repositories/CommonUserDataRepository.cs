07cfd83adccf59cd731e2964c81707ec0ffe0d55
comougi <spsprwrw@gmail.com>
2020-06-13 17:35:37 +0300

    add CommonUserDataRepository

---
+++ Infrastructure/Repositories/CommonUserDataRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Entities;
+using Infrastructure.CommonRepositories;
+using Infrastructure.IRepositories;
+
+namespace Infrastructure.Repositories
+{
+    public class CommonUserDataRepository : DbRepository<CommonUserData>, ICommonUserDataRepository
+
+    {
+        public CommonUserDataRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public void Add(CommonUserData entity)
+        {
+            DbAdd(entity);
+            DbSaveChanges();
+        }
+
+        public CommonUserData GetById(int id)
+        {
+            return DbGetById(id);
+        }
+
+        public void Update(CommonUserData entity)
+        {
+            DbUpdate(entity);
+            DbSaveChanges();
+        }
+
+        public void Remove(CommonUserData entity)
+        {
+            DbRemove(entity);
+            DbSaveChanges();
+        }
+
+        public ICollection<CommonUserData> GetAll()
+        {
+            return DbContext.CommonUserDatas.ToList();
+        }
+    }
+}
