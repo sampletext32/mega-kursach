ee13f61e738cc33bf5551e0295841c543d368dfc
comougi <spsprwrw@gmail.com>
2020-06-13 17:34:07 +0300

    add ArtistDataRepository

---
+++ Infrastructure/Repositories/ArtistDataRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Entities;
+using Infrastructure.CommonRepositories;
+using Infrastructure.IRepositories;
+
+namespace Infrastructure.Repositories
+{
+    public class ArtistDataRepository : DbRepository<ArtistData>, IArtistDataRepository
+    {
+        public ArtistDataRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public void Add(ArtistData entity)
+        {
+            DbAdd(entity);
+            DbSaveChanges();
+        }
+
+        public ArtistData GetById(int id)
+        {
+            return DbGetById(id);
+        }
+
+        public void Update(ArtistData entity)
+        {
+            DbUpdate(entity);
+            DbSaveChanges();
+        }
+
+        public void Remove(ArtistData entity)
+        {
+            DbRemove(entity);
+            DbSaveChanges();
+        }
+
+        public ICollection<ArtistData> GetAll()
+        {
+            return DbContext.ArtistDatas.ToList();
+        }
+    }
+}
