6c626d32f96f5d4a1f961d2c2ec52856955bc4e0
comougi <spsprwrw@gmail.com>
2020-06-13 17:37:26 +0300

    add DistributorDataRepository

---
+++ Infrastructure/Repositories/DistributorDataRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Entities;
+using Infrastructure.CommonRepositories;
+using Infrastructure.IRepositories;
+
+namespace Infrastructure.Repositories
+{
+    public class DistributorDataRepository: DbRepository<DistributorData>,IDistributorDataRepository
+
+    {
+        public DistributorDataRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public void Add(DistributorData entity)
+        {
+            DbAdd(entity);
+            DbSaveChanges();
+        }
+
+        public DistributorData GetById(int id)
+        {
+            return DbGetById(id);
+        }
+
+        public void Update(DistributorData entity)
+        {
+            DbUpdate(entity);
+            DbSaveChanges();
+        }
+
+        public void Remove(DistributorData entity)
+        {
+            DbRemove(entity);
+            DbSaveChanges();
+        }
+
+        public ICollection<DistributorData> GetAll()
+        {
+            return DbContext.DistributorDatas.ToList();
+        }
+    }
+}
