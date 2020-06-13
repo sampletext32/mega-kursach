3bb4dae761004a725888b4d40220794065d28ace
comougi <spsprwrw@gmail.com>
2020-06-13 17:38:44 +0300

    add PlaylistRepository

---
+++ Infrastructure/Repositories/PlaylistRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Entities;
+using Infrastructure.CommonRepositories;
+using Infrastructure.IRepositories;
+
+namespace Infrastructure.Repositories
+{
+    public class PlaylistRepository : DbRepository<Playlist>, IPlaylistRepository
+    {
+        public PlaylistRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public void Add(Playlist entity)
+        {
+            DbAdd(entity);
+            DbSaveChanges();
+        }
+
+        public Playlist GetById(int id)
+        {
+            return DbGetById(id);
+        }
+
+        public void Update(Playlist entity)
+        {
+            DbUpdate(entity);
+            DbSaveChanges();
+        }
+
+        public void Remove(Playlist entity)
+        {
+            DbRemove(entity);
+            DbSaveChanges();
+        }
+
+        public ICollection<Playlist> GetAll()
+        {
+            return DbContext.Playlists.ToList();
+        }
+    }
+}
