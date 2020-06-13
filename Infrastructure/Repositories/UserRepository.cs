1261913106deb777bbbf85a99626d2637c40b349
comougi <spsprwrw@gmail.com>
2020-06-13 17:41:19 +0300

    add UserRepository

---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Entities;
+using Infrastructure.CommonRepositories;
+using Infrastructure.IRepositories;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository : DbRepository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public void Add(User entity)
+        {
+            DbAdd(entity);
+            DbSaveChanges();
+        }
+
+        public User GetById(int id)
+        {
+            return DbGetById(id);
+        }
+
+        public void Update(User entity)
+        {
+            DbUpdate(entity);
+            DbSaveChanges();
+        }
+
+        public void Remove(User entity)
+        {
+            DbRemove(entity);
+            DbSaveChanges();
+        }
+
+        public ICollection<User> GetAll()
+        {
+            return DbContext.Users.ToList();
+        }
+    }
+}
