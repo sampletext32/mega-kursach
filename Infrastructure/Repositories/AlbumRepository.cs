bbaeb06e4b06e511bf79c79b8e48f3e3fd1a439c
comougi <spsprwrw@gmail.com>
2020-06-13 17:30:05 +0300

    add AlbumRepository, TrackRepository, GenreRepository

---
+++ Infrastructure/Repositories/AlbumRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Entities;
+using Infrastructure.CommonRepositories;
+using Infrastructure.IRepositories;
+
+namespace Infrastructure.Repositories
+{
+    public class AlbumRepository : DbRepository<AlbumRepository>, IAlbumRepository
+    {
+        public AlbumRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public void Add(Album entity)
+        {
+            DbAdd(entity);
+            DbSaveChanges();
+        }
+
+        public Album GetById(int id)
+        {
+            return DbGetById(id);
+        }
+
+        public void Update(Album entity)
+        {
+            DbUpdate(entity);
+            DbSaveChanges();
+        }
+
+        public void Remove(Album entity)
+        {
+            DbRemove(entity);
+            DbSaveChanges();
+        }
+
+        public ICollection<Album> GetAll()
+        {
+            return DbContext.Albums.ToList();
+        }
+    }
+}
---
+++ Infrastructure/Repositories/TrackRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Entities;
+using Infrastructure.CommonRepositories;
+using Infrastructure.IRepositories;
+
+namespace Infrastructure.Repositories
+{
+    public class TrackRepository : DbRepository<Track>, ITrackRepository
+    {
+        public TrackRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public void Add(Track entity)
+        {
+            DbAdd(entity);
+            DbSaveChanges();
+        }
+
+        public Track GetById(int id)
+        {
+            return DbGetById(id);
+        }
+
+        public void Update(Track entity)
+        {
+            DbUpdate(entity);
+            DbSaveChanges();
+        }
+
+        public void Remove(Track entity)
+        {
+            DbRemove(entity);
+            DbSaveChanges();
+        }
+
+        public ICollection<Track> GetAll()
+        {
+            return DbContext.Tracks.ToList();
+        }
+    }
+}
